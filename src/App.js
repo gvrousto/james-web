aa53fb5938f4a6e8f3fe4abab417aef488dbc9ae
Gus Vroustouris <gvroustouris@gmail.com>
2021-06-22 22:27:40 -0400

    contact is done I guess

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import Navbar from "./navbar/Navbar";
 import About from "./about/About";
+import Contact from "./contact/Contact";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
             <About />
           </Route>
           <Route path="/contact">
-            <div>Contact</div>
+            <Contact />
           </Route>
           <Route path="/">
             <div>Home</div>
---
+++ src/contact/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <div className="contact-container">
+      <div className="contact-content">
+        Currently open for furniture and object commissions. Please inquire at
+        Jclangford97@gmail.com
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
